66c4e7fbc1f6584cb2c39ba7bb0acab53bd7425e
Jacob Stilin <stilinj@gmail.com>
2019-10-29 11:35:29 -0500

    Created models and controllers for employees and customers

---
+++ Trash/Models/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Trash.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
+
+        [Display(Name = "Pick Up Day")]
+        public DateTime PickUpDay { get; set; }
+
+        [Display(Name = "Balance")]
+        public double Balance { get; set; }
+
+        [Display(Name = "Monthly Charge")]
+        public double MonthlyCharge { get; set; }
+
+        [Display(Name = "Pick Up Confirmed")]
+        public bool PickUpConfirmed { get; set; }
+
+        [Display(Name = "Start")]
+        public DateTime Start { get; set; }
+
+        [Display(Name = "End")]
+        public DateTime End { get; set; }
+    }
+}
--- Trash/Models/IdentityModels.cs
+++ Trash/Models/IdentityModels.cs
@@ -20,6 +20,10 @@ namespace Trash.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -29,5 +33,7 @@ namespace Trash.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<Trash.Models.Employee> Employees { get; set; }
     }
 }
